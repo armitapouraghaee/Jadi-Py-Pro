e5cfa4c6f9b0d91f05a6f1bbc6ed26828830de5e
Rmita <armitapouraghaee@gmail.com>
2025-07-23 16:13:28 +0330

    commit3

--- OOP/simple-opp.py
+++ OOP/simple-opp.py
@@ -5,5 +5,12 @@ class person:
         self.age = age
     def get_name(self):
         print('name is %s' %self.name)
+    def get_age(self):
+        print('she is %s' %self.age)
+    #or
+    def get_info(self):
+      print('name is %s and age is %i' %(self.name , self.age))
 Armita = person ("Armita" , 24)
 Armita.get_name()
+Armita.get_age()
+Armita.get_info()

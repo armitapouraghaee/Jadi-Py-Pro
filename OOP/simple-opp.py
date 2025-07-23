02618ddaf371f71298ba99dcd6acc4e56e18fe05
Rmita <armitapouraghaee@gmail.com>
2025-07-23 16:24:38 +0330

    commit4

--- OOP/simple-opp.py
+++ OOP/simple-opp.py
@@ -3,6 +3,7 @@ class person:
     def __init__(self,name,age):
         self.name = name
         self.age = age
+        person.count =person.count +1
     def get_name(self):
         print('name is %s' %self.name)
     def get_age(self):
@@ -10,7 +11,11 @@ class person:
     #or
     def get_info(self):
       print('name is %s and age is %i' %(self.name , self.age))
+    def return_count(self):
+        return (person.count)
 Armita = person ("Armita" , 24)
 Armita.get_name()
 Armita.get_age()
 Armita.get_info()
+
+print('at the moment i have %i perons' %Armita.return_count())

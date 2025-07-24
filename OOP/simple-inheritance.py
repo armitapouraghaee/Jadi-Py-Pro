fe02dd647a510f845b94dbceabe283d4356e332c
Rmita <armitapouraghaee@gmail.com>
2025-07-24 13:32:21 +0330

    commit4

--- OOP/simple-inheritance.py
+++ OOP/simple-inheritance.py
@@ -1,17 +1,23 @@
 class Computer:
     count = 0
     def __init__(self, ram, hard, cpu):
+        Computer.count +=1
         self.ram = ram
         self.hard = hard
         self.cpu = cpu
     def value(self):
         return self.ram+self.hard+self.cpu
+    def __del__(self):
+        Computer.count -=1
 class Laptop(Computer):
     def value(self):
         return self.ram+self.hard+self.cpu+self.size
 
+#***class Ipad(Computer,Laptop)***
+
 pc1=Computer(12,2,4)
 print(pc1.value())
+del pc1
 
 laptop1 = Laptop(16,2,4)
 laptop1.size = 13

c592a78721c299abf4573019d8876583c4ea6644
Rmita <armitapouraghaee@gmail.com>
2025-07-26 18:41:16 +0330

    add-player

--- OOP/Football-Project.py
+++ OOP/Football-Project.py
@@ -1,4 +1,14 @@
+import random
 class Person:
-    def __init__(self.name):
+    def __init__(self,name):
         self.name = name 
-        
+class Footballer(Person):
+    def __init__(self,name):
+        super().__init__(name)
+           
+names_str="حسین - مازیار - اکبر - نیما -  مهدی - فرهاد - محمد - خشایار - میلاد - مصطفی - امین - سعید - پویا - پوریا - رضا - علی - بهزاد - سهیل - بهروز - شهروز - سامان - محسن"
+names = names_str.split(" - ")
+players= []
+for name in names:
+    player = Footballer(name)
+    players.append(player)
